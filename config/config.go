ae21bd0ac1ef3caeb4d83b54eefd9a3eba8f53a7
AmruthSD <amruth1908@gmail.com>
2025-04-11 21:13:00 +0530

    generating node id

--- config/config.go
+++ config/config.go
@@ -1,22 +1,34 @@
 package config
 
 import (
+	"crypto/sha256"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 )
 
 type metadata struct {
-	Type int
+	Type   int
+	NodeID [32]byte
 }
 
 var MetaData metadata
 
-func ReadFlags() {
-	var type_flag string
+func InitConfig() {
+	var type_flag, restart string
 	flag.StringVar(&type_flag, "type", "none", "Type of node either storage/lookup")
-
+	flag.StringVar(&restart, "restart", "none", "Restarting or completely new")
 	flag.Parse()
+
+	if restart == "flase" {
+		// TODO read config from a file and fill metadata
+
+		return
+	} else if restart != "none" {
+		fmt.Println("Unknown restart flag")
+		os.Exit(1)
+	}
 
 	if type_flag == "storage" {
 		MetaData.Type = 1
@@ -29,4 +41,17 @@ func ReadFlags() {
 		fmt.Println("type flag not recognised")
 		os.Exit(1)
 	}
+	MetaData.generate_new_node_id()
+}
+
+func (MetaData *metadata) generate_new_node_id() {
+	bytes_random := make([]byte, 32)
+	_, err := rand.Read(bytes_random)
+	if err != nil {
+		fmt.Println("error in random read")
+		os.Exit(1)
+	}
+
+	MetaData.NodeID = sha256.Sum256(bytes_random)
+
 }
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	config.ReadFlags()
+	config.InitConfig()
 }
