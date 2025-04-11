8c95c5768edac9c36eda3111d0c52de5850c2360
AmruthSD <amruth1908@gmail.com>
2025-04-11 22:07:21 +0530

    added functionality for starting node connections

--- connection/tcp.go
+++ connection/tcp.go
@@ -1,7 +1,40 @@
 package connection
 
-import "net"
+import (
+	"fmt"
+	"net"
+	"strconv"
+
+	"github.com/AmruthSD/Decentralized-Distributed-Files/config"
+)
 
 type Node struct {
-	conn net.Conn
+	conn              net.Conn
+	listening_address string
+	parent_connector  Connector
+}
+
+type Connector interface {
+	handel_conn(net.Conn)
+}
+
+func (node *Node) start() error {
+	l, err := net.Listen("tcp", "0.0.0.0:"+strconv.Itoa(int(config.MetaData.Port)))
+	if err != nil {
+		return err
+	}
+
+	node.listening_address = l.Addr().String()
+
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+
+		go node.parent_connector.handel_conn(conn)
+	}
 }
